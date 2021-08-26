213749f5f16ac633bba51a0f4140af4524a4ab2c
mkshgh <52674161+mkshgh@users.noreply.github.com>
2021-08-26 17:36:31 +0545

    remove package_dir={'','src'},

--- setup.py
+++ setup.py
@@ -4,30 +4,32 @@ import os
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-VERSION = '0.0.13'
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+VERSION = '0.0.1'
 DESCRIPTION = 'Python Resize Pixel'
 LONG_DESCRIPTION = open('README.md').read().strip()
 
+
+
 # Setting up
-setup(
-    name='resizePixel',
+setup(name='resizePixel',
     version=Version('1.0.0').number,
     author="mkshgh",
     author_email="<mukesh.ghimire@outlook.com>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
-    long_description=LONG_DESCRIPTION,
+    long_description=long_description,
     url='mghimire.com.np',
     packages=find_packages(),
-    package_dir={'','src'},
-    install_requires=['pytest', 'pytest-cov', 'PIL'],
+    install_requires=['pytest', 'pytest-cov'],
     keywords=['python', 'image', 'image quality', 'reduce', 'increase', 'frustrated'],
     classifiers=[
-        "Development Status :: 1 - Testing",
-        "Intended Audience :: Dreamers",
         "Programming Language :: Python :: 3",
-        "Operating System :: Unix",
-        "Operating System :: MacOS :: MacOS X",
-        "Operating System :: Microsoft :: Windows",
-    ]
-)
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>3.7',
+    )
